70e2140985064aaec40b776e2ae2d914f473d227
Viince64 <Vincentcrpg10@gmail.com>
2020-01-17 10:19:33 +0100

    maj

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
-	def show
-    	@themes = Theme.all
-  	end
+
+	def navigation_themes
+		Theme.all
+	end
+	helper_method :navigation_themes
 end
